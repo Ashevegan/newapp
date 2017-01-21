62d34ed0f1ef82565558792ff780b902feb66449
“Ashley <theashley.elyse@gmail.com>
2017-01-21 18:38:22 -0500

    init

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,9 @@ Rails.application.routes.draw do
 
   get 'static_pages/contact'
 
-  get 'static_pages/index'
+  get 'static_pages/products'
 
-  root 'static_pages#index'
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
