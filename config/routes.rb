5295b547940390ce0d9da7686b9a6bf13d158c14
“Ashley <theashley.elyse@gmail.com>
2017-02-03 17:36:20 -0500

    fixed routes error

--- config/routes.rb
+++ config/routes.rb
@@ -3,19 +3,19 @@
 
   #devise_for :users, :controllers => { :registrations => "user_registrations" }
   #resources :products do
-    r#esources :comments
+    #resources :comments
   #end
   #resources :users
 
-  get 'static_pages/index'
+  #get 'static_pages/index'
 
-  get 'static_pages/about'
+  #get 'static_pages/about'
 
-  get 'static_pages/contact'
+  #get 'static_pages/contact'
 
-  get 'static_pages/landing_page'
+  #get 'static_pages/landing_page'
 
-  root 'static_pages#landing_page'
+  #root 'static_pages#landing_page'
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
