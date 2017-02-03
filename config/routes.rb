9928112327af79fe2b4fa4eca83abb368830855e
“Ashley <theashley.elyse@gmail.com>
2017-02-02 19:30:30 -0500

    gems

--- config/routes.rb
+++ config/routes.rb
@@ -15,10 +15,6 @@ Rails.application.routes.draw do
 
   get 'static_pages/landing_page'
 
-  post 'static_pages/thank_you'
-
-  post 'payments/create'
-
   root 'static_pages#landing_page'
 
   resources :orders, only: [:index, :show, :create, :destroy]
