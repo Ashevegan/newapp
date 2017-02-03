6dc88bbfc19237d6571e6a96fe4e1a09cef218a1
“Ashley <theashley.elyse@gmail.com>
2017-02-02 19:05:20 -0500

    precompiled assets

--- Gemfile
+++ Gemfile
@@ -54,5 +54,24 @@ end
 
 
 
+
+
+
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+
+gem 'devise'
+
+gem 'cancancan', '~> 1.10'
+
+gem 'will_paginate', '~> 3.1.0'
+
+gem 'rails-controller-testing'
+
+gem 'stripe'
+
+gem 'brakeman'
+
+gem 'redis-rails'
+
+gem 'newrelic_rpm'
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,29 @@
+
 Rails.application.routes.draw do
-  get 'pages/index'
-  root 'pages#index'
+
+  devise_for :users, :controllers => { :registrations => "user_registrations" }
+  resources :products do
+    resources :comments
+  end
+  resources :users
+
+  get 'static_pages/index'
 
   get 'static_pages/about'
 
   get 'static_pages/contact'
 
-  get 'static_pages/products'
+  get 'static_pages/landing_page'
 
+  post 'static_pages/thank_you'
 
+  post 'payments/create'
+
+  root 'static_pages#landing_page'
+
+  resources :orders, only: [:index, :show, :create, :destroy]
+
+  mount ActionCable.server => '/cable'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
