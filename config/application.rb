df75c5ebcbef8a80e656352a4f40e51b17c3453e
“Ashley <theashley.elyse@gmail.com>
2017-02-04 14:04:23 -0500

    app fix

--- config/application.rb
+++ config/application.rb
@@ -1,15 +1,8 @@
 require_relative 'boot'
 
-require "rails"
+require 'rails/all'
 # Pick the frameworks you want:
-require "active_model/railtie"
-require "active_job/railtie"
-require "active_record/railtie"
-require "action_controller/railtie"
-require "action_mailer/railtie"
-require "action_view/railtie"
-require "action_cable/engine"
-require "sprockets/railtie"
+
 # require "rails/test_unit/railtie"
 
 
@@ -23,6 +16,6 @@ module Nameofapp
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    config.serve_static_assets = true
+ 
   end
 end
