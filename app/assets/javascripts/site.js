ef15b1e661b4e6489a326c62ecead40fed5ea073
“Ashley <theashley.elyse@gmail.com>
2017-04-20 17:58:27 -0400

    added elevate zoom

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,9 +1,16 @@
-$(document).on('turbolinks:load', function(){
-    $('.rating').raty( { path: '/assets', scoreName: 'comment[rating]' });
-    $('.rated').raty({ path: '/assets',
-      readOnly: true,
-      score: function() {
-        return $(this).attr('data-score');
-      }
-    });
+
+var refreshRating = function(){
+	$('.rating').raty({path: '/assets', scoreName: 'comment[rating]'});
+	$('.rated').raty({path: '/assets',
+		readOnly: true,
+		score: function(){
+			return $(this).attr('data-score');
+		}
+	}); //Raty Plugin
+};
+
+$(document).on('turbolinks:load ajaxSuccess', function(){
+	refreshRating(); // Raty Plugin Call
+	
+	$('.img-zoom').elevateZoom(); //Elevate Zoom Plugin
 });
