f2a8ea8e6c071f3ba1832d13f4d5dad1c941daf0
“Ashley <theashley.elyse@gmail.com>
2017-04-20 18:21:24 -0400

    Using AJAX to create comments remotely

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,16 +1,15 @@
-
-var refreshRating = function(){
-	$('.rating').raty({path: '/assets', scoreName: 'comment[rating]'});
-	$('.rated').raty({path: '/assets',
-		readOnly: true,
-		score: function(){
-			return $(this).attr('data-score');
-		}
-	}); //Raty Plugin
+var refreshRating = function() {
+    $('.rating').raty( { path: '/assets', scoreName: 'comment[rating]' });
+    $('.rated').raty({ path: '/assets',
+        readOnly: true,
+        score: function() {
+            return $(this).attr('data-score');
+        }
+    });
 };
 
-$(document).on('turbolinks:load ajaxSuccess', function(){
-	refreshRating(); // Raty Plugin Call
-	
-	$('.img-zoom').elevateZoom(); //Elevate Zoom Plugin
+$(document).on('turbolinks:load', function() {
+    refreshRating();
+
+    $('.img-zoom').elevateZoom();
 });
