fa96d5814cac970b07cabc3c26520e1e08d01de5
“Ashley <theashley.elyse@gmail.com>
2017-04-21 16:26:23 -0400

    6.14 ActiveJob

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,9 +2,10 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :product
 
-validates :body, presence: true
+  validates :body, presence: true
   validates :user, presence: true
   validates :product, presence: true
   validates :rating, numericality: { only_integer: true }
+  after_create_commit { CommentUpdateJob.perform_later(self, @user) }
 end
  
