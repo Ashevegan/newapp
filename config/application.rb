5372d969df06c29a0a433a12887fa18bc4bdf794
“Ashley <theashley.elyse@gmail.com>
2017-04-23 12:45:44 -0400

    fixed application.rb

--- config/application.rb
+++ config/application.rb
@@ -3,7 +3,7 @@
 require 'rails/all'
 # Pick the frameworks you want:
 
-config.cache_store = :redis_store, 'redis://localhost:6379/0/cache'
+
 
 
 
@@ -15,11 +15,15 @@ config.cache_store = :redis_store, 'redis://localhost:6379/0/cache'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+
 module Nameofapp
   class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
+    # Settin
+    #
+    # gs in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
- 
+    config.cache_store = :redis_store, 'redis://localhost:6379/0/cache'
+    config.assets.initialize_on_precompile = false
   end
 end
