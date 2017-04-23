997c0babf84d4b45a1f4aa1cd1e56a2b734d7f68
“Ashley <theashley.elyse@gmail.com>
2017-04-23 12:49:46 -0400

    mailers update

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,18 @@
+
 class UserMailer < ApplicationMailer
-def welcome(user)
-  @appname = "Bike Shop"
-  mail( :to => user.email,
-        :subject => "Welcome to #{@appname}!")
+  default from: "from@example.com"
+
+  def contact_form(email, name, message)
+    @message = message
+    mail(:from => email,
+         :to => 'theashley.elyse@gmail.com',
+         :subject => "A new contact form message from #{name}")
+  end
+
+  def welcome(user)
+    @appname = "Bike Nyc"
+    mail( :to => user.email,
+          :subject => "Welcome to #{@appname}!")
+  end
+
 end
