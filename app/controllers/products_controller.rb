bca20a1dcf11dfbaad9f6954bb6fbfb9be3930a1
“Ashley <theashley.elyse@gmail.com>
2017-05-29 14:20:52 -0400

    product params fix

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -80,7 +80,7 @@ end
 
   # Never trust parameters from the scary internet, only allow the white list through.
   def product_params
-    params.require(:product).permit(:name, :description, :image_url, :price)
+    params.require(:product).permit(:name, :description, :image_url, :color, :price)
   end
 end
 
