d4a165972d80fb9d97110e66b8f74c99ca18d41b
“Ashley <theashley.elyse@gmail.com>
2017-05-29 13:57:30 -0400

    ajax comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,11 +4,9 @@ class CommentsController < ApplicationController
     @product = Product.find(params[:product_id])
     @comment = @product.comments.new(comment_params)
     @comment.user = current_user
-    @user = current_user
-      respond_to do |format|
-        if @comment.save
-        #ActionCable.server.broadcast 'product_channel', comment: @comment, average_rating: @comment.product.average_rating
-        #ProductChannel.broadcast_to @product.id, comment: CommentsController.render(partial: 'comments/comment', locals: {comment: @comment}), average_rating: @product.average_rating
+    @comment.save
+    respond_to do |format|
+      if @comment.save
         format.html { redirect_to @product, notice: 'Review was created successfully.' }
         format.json { render :show, status: :created, location: @product }
         format.js
@@ -23,12 +21,13 @@ class CommentsController < ApplicationController
     @comment = Comment.find(params[:id])
     product = @comment.product
     @comment.destroy
-    redirect_to product
+    redirect_to product, only_path: true
   end
+
 
   private
 
-  def comment_params
-    params.require(:comment).permit(:user_id, :body, :rating)
-  end
+    def comment_params
+      params.require(:comment).permit(:user_id, :body, :rating)
+    end
 end
