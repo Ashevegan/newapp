e24e0bd8e1e444c8e7f9d1b8a8b8e262ea546dd2
“Ashley <theashley.elyse@gmail.com>
2017-05-31 17:36:29 -0400

    adding more products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -20,7 +20,7 @@ end
   # GET /products/1.json
 
   def show
-    @comments = @product.comments.order("created_at DESC").paginate(:page => params[:page], :per_page => 3)
+    @comments = @product.comments.order("created_at DESC").paginate(:page => params[:page], :per_page => 6)
 
   end
 
